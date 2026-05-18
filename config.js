0c5150dc34c68acedcefc0782b8b0daeed63437f
Nikola Kolev <92107268+nikolakolev1@users.noreply.github.com>
2026-05-18 16:01:10 +0300

    CHANGE URL

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 // 2) Копирай URL адреса, който завършва на /exec.
 // 3) Постави го тук между кавичките.
 window.APP_CONFIG = {
-  WEB_APP_URL: "https://script.google.com/macros/s/AKfycbxs4x0_rJS8CD9nvoyLfiWIImbKiiaybG-lfVr7dS6N_XnEkykyqGY1gkp1b2bbidP-1w/exec"
+  WEB_APP_URL: "https://script.google.com/macros/s/AKfycbwRHAb0XLYhpf5ZsH6zM9jqGQ830yICsDsXVylz8Nb-ll8jiY3RhT2pkRIuUvCxpfja8Q/exec"
 };
